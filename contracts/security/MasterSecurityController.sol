866270826111f3ad62740f907f9d13dd8b8d3cb2
Xmoohad <xmoohad@epic-chain.org>
2025-07-14 13:01:22 +0100

    EpicStarter Presale - EpicChain Labs

--- contracts/security/MasterSecurityController.sol
+++ contracts/security/MasterSecurityController.sol
@@ -10,7 +10,7 @@ import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-import "./interfaces/IEnhancedSecurityInterface.sol";
+import "../interfaces/IEnhancedSecurityInterface.sol";
 import "./advanced/AdvancedCryptographicSecurityManager.sol";
 import "./quantum/QuantumResistantCryptography.sol";
 import "./multisig/AdvancedMultiSigSecurity.sol";
